f63f4ec31172fea6f74ff2e62c48aa53970af9bd
Alfredo Delgado L <aadelop@gmail.com>
2024-08-19 09:48:42 +0200

    Added cors middleware to the server to fix the CORS explorer problem

--- back/index.ts
+++ back/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import {Request,Response} from 'express';
 import { request } from 'http';
+import cors from 'cors'
 
 const app = express()
 const port = 3333;
 app.use(express.json())
+app.use(cors())
 
 app.get('/:p1/:p2', (req: Request, res:Response) => {
     const {p1,p2} = req.params;
