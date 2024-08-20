bbc2a4be04b00216f9138b6f107bd973da0c21e8
Alfredo Delgado L <aadelop@gmail.com>
2024-08-20 09:38:15 +0200

    Added direct call to the node from react without  3er libraries

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,8 +2,9 @@ import { Button } from "@/components/ui/button"
 import { useEffect, useState } from "react"
 
 type Data = {
-  p1: string
-  p2: string
+  jsonrpc: string
+  id: number
+  result: string
 }
 
 export default function Home() {
@@ -11,16 +12,32 @@ export default function Home() {
   const [data, setData] = useState<Data | null>(null)
 
   useEffect(() => {
-    fetch('http://localhost:3333/1111/2222')
+    fetch('http://localhost:5556',{
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'eth_getBalance',
+        params: [
+          '0xc9958ed67E5196b8d99cC3C3a0B83f832c5EFB9a',
+          'latest'
+        ],
+        id: 1
+      })
+    })
     .then(res => res.json())
-    .then(data => setData(data))
+    .then(setData)
+    .catch(console.error)
+   
   }, [])
 
   if(!data) return <div>Loading...</div>
 
   return (
     <div>
-      {data.p1}{data.p2}
+      {Number(data.result)/10**18}
     </div>
   )
 }
