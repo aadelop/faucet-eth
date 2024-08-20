70865a242189c00ff768c24a6e664d4092d254dc
Alfredo Delgado L <aadelop@gmail.com>
2024-08-20 10:47:09 +0200

    Included api balance route that get balance of address from the created eth node

--- back/index.ts
+++ back/index.ts
@@ -8,9 +8,26 @@ const port = 3333;
 app.use(express.json())
 app.use(cors())
 
-app.get('/:p1/:p2', (req: Request, res:Response) => {
-    const {p1,p2} = req.params;
-    res.send({p1,p2})
+app.get('/api/balance/:address', async (req: Request, res:Response) => {
+    const {address} = req.params;
+    const freturn = await fetch('http://localhost:5556',{
+        method:'POST',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          jsonrpc: '2.0',
+          method: 'eth_getBalance',
+          params: [
+            address,
+            'latest'
+          ],
+          id: 1
+        })
+      })
+      const data:any = await freturn.json();
+      res.json({address,balance:Number(data.result)/10**18, date: new Date().toISOString()})
+
 })
 
 app.post('/', (req: Request, res:Response) => {
@@ -19,5 +36,5 @@ app.post('/', (req: Request, res:Response) => {
 })
 
 app.listen(port, ()=>{
-    console.log("Server is runing on port ${port}");
+    console.log(`Server is runing on port ${port}`);
 })
