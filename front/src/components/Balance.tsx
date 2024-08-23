c52c3bd2f7a9ce025d7a838b815f5a4342bd7363
Alfredo Delgado L <aadelop@gmail.com>
2024-08-23 09:20:49 +0200

    Added logic and imports to show the balance of the actual account selected on metamask"

--- front/src/components/Balance.tsx
+++ front/src/components/Balance.tsx
@@ -1,3 +1,24 @@
+import { UserContext } from "@/App";
+import { useContext, useEffect, useState } from "react";
+
 export function Balance(){
-    return <div>Balance</div>
+    const {state, setState} = useContext(UserContext);
+    const [balance, setBalance] = useState<number>(0);
+   
+    useEffect(() => {
+        const ethereum = window.ethereum;
+        if (ethereum == null) {
+            alert("Please install metamask");
+            return;
+        }
+        ethereum.request({ method: "eth_getBalance", params:[state.acc]})
+        .then((data:string) => {
+            setBalance(Number(data)/10**18)
+        })
+    }, [state.acc])
+   
+    return  <div>
+                <h1>Balance</h1>
+                <p>The address <b>{state.acc}</b> have: <b>{balance} CC</b> </p>
+            </div>
 }
